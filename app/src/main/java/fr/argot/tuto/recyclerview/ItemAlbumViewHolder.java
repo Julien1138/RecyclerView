aca40f9122531e688d926a4790bc5f4c800a3de3
Julien1138 <julien.aupart@gmail.com>
2016-11-12 14:21:37 +0100

    Ajout des "TextDrawable" en début de ligne

--- app/src/main/java/fr/argot/tuto/recyclerview/ItemAlbumViewHolder.java
+++ app/src/main/java/fr/argot/tuto/recyclerview/ItemAlbumViewHolder.java
@@ -1,15 +1,21 @@
 package fr.argot.tuto.recyclerview;
 
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
 
 /**
  * Created by Julien on 11/11/2016.
  */
 
 public class ItemAlbumViewHolder extends RecyclerView.ViewHolder {
-    // Les 3 éléments de la vue
+    // Les 4 éléments de la vue
+    private ImageView mIcon;
     private TextView mTitle;
     private TextView mDepartureDate;
     private TextView mDuration;
@@ -21,6 +27,7 @@ public class ItemAlbumViewHolder extends RecyclerView.ViewHolder {
         super(itemView);
 
         // Ici on associe les vues à leurs pointeurs
+        this.mIcon = (ImageView) itemView.findViewById(R.id.album_item_icon);
         this.mTitle = (TextView) itemView.findViewById(R.id.album_item_title);
         this.mDepartureDate = (TextView) itemView.findViewById(R.id.album_item_departure_date);
         this.mDuration = (TextView) itemView.findViewById(R.id.album_item_duration);
@@ -28,9 +35,15 @@ public class ItemAlbumViewHolder extends RecyclerView.ViewHolder {
         this.view = itemView;
     }
 
-    // Les 3 setters pour chacun des 3 éléments de la vue
+    // Les 3 setters pour chacun des 3 chanmps texte de la vue
     public void setTitle(String myTitle) {
         this.mTitle.setText(myTitle);
+
+        // On mets ici la génération de l'icon
+        ColorGenerator generator = ColorGenerator.DEFAULT;
+        TextDrawable drawable = TextDrawable.builder()
+                .buildRound(myTitle.substring(0, 1), ColorGenerator.DEFAULT.getColor(myTitle));
+        mIcon.setImageDrawable(drawable);
     }
 
     public void setDepartureDate(String myDepartureDate) {
